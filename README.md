8b7d2f00a11f0e195f911c9a4301dff487bce9d1
Maartje Eyskens <meyskens@me.com>
2016-04-11 17:39:45 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# :earth_africa: alpine-glibc
+
+![](https://raw.githubusercontent.com/multiarch/dockerfile/master/logo.jpg)
+
+Multiarch alpine images for Docker with glibc. Based on `frolvlad/alpine-glibc`'s work
+
+* https://imagelayers.io/?images=meyskens%2Falpine-glibc:armhf-edge,meyskens%2Falpine-glibc:x86-edge,meyskens%2Falpine-glibc:x86_64-edge
+
+* `multiarch/alpine` on [Docker Hub](https://hub.docker.com/r/meyskens/alpine-glibc/)
+* [Available tags](https://hub.docker.com/r/meyskens/alpine-glibc/tags/)
+
+## Usage
+
+Once you need to configure binfmt-support on your Docker host.
+This works locally or remotely (i.e using boot2docker or swarm).
+
+```console
+# configure binfmt-support on the Docker host (works locally or remotely, i.e: using boot2docker)
+$ docker run --rm --privileged multiarch/qemu-user-static:register --reset
+```
+
+Then you can run an `armhf` image from your `x86_64` Docker host.
+
+```console
+$ docker run -it --rm multiarch/alpine:armhf-edge /bin/sh
+/ # uname -a
+Linux a0818570f614 4.1.13-boot2docker #1 SMP Fri Nov 20 19:05:50 UTC 2015 armv7l armv7l armv7l GNU/Linux
+```
+
+Or an `x86_64` image from your `x86_64` Docker host, directly, without qemu emulation.
+
+```console
+$ docker run -it --rm multiarch/alpine:amd64-edge /bin/sh
+/ # uname -a
+Linux 27fe384370c9 4.1.13-boot2docker #1 SMP Fri Nov 20 19:05:50 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux
+```
+
+It also works for `x86`
+
+```console
+$ docker run -it --rm multiarch/alpine:x86-edge /bin/sh
+/ # uname -a
+Linux 1ae459268bce 3.13.0-36-generic #63-Ubuntu SMP Wed Sep 3 21:30:07 UTC 2014 x86_64 Linux
+```
+
+## License
+
+MIT
---
+++ update.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+set -e
+
+cd "$(dirname "$BASH_SOURCE")"
+
+versions=( "$@" )
+if [ ${#versions[@]} -eq 0 ]; then
+	versions=( */ )
+fi
+versions=( "${versions[@]%/}" )
+
+repo="meyskens/alpine-glibc"
+
+for version in "${versions[@]}"; do
+    (
+	cd "$version"
+
+	REL="$(cat version)"
+	ARCH="$(cat arch)"
+	qemu_arch="$(cat qemu_arch || true)"
+
+	MIRROR=${MIRROR:-http://dl-cdn.alpinelinux.org/alpine}
+	REPO=$MIRROR/$REL/main
+	TMP=tmp
+	ROOTFS=rootfs
+
+	mkdir -p $TMP $ROOTFS/usr/bin
+
+	# download apk.static
+	if [ ! -f $TMP/sbin/apk.static ]; then
+	    apkv=$(curl -sSL $REPO/$ARCH/APKINDEX.tar.gz | tar -Oxz | strings |
+		grep '^P:apk-tools-static$' -A1 | tail -n1 | cut -d: -f2)
+	    curl -sSL $REPO/$ARCH/apk-tools-static-${apkv}.apk | tar -xz -C $TMP sbin/apk.static
+	fi
+
+	# FIXME: register binfmt
+
+	# install qemu-user-static
+	if [ -n "${qemu_arch}" ]; then
+	    if [ ! -f x86_64_qemu-${qemu_arch}-static.tar.xz ]; then
+		wget -N https://github.com/multiarch/qemu-user-static/releases/download/v2.5.0/x86_64_qemu-${qemu_arch}-static.tar.xz
+	    fi
+	    tar -xvf x86_64_qemu-${qemu_arch}-static.tar.xz -C $ROOTFS/usr/bin/
+	fi
+
+	# create rootfs
+	$TMP/sbin/apk.static --repository $REPO --update-cache --allow-untrusted \
+	    --root $ROOTFS --initdb add alpine-base --verbose
+
+	# alter rootfs
+	printf '%s\n' $REPO > $ROOTFS/etc/apk/repositories
+
+	# create tarball of rootfs
+	if [ ! -f rootfs.tar.xz ]; then
+	    tar --numeric-owner -C $ROOTFS -c . | xz > rootfs.tar.xz
+	fi
+
+	# clean rootfs
+	rm -f $ROOTFS/usr/bin/qemu-*-static
+
+	# create Dockerfile
+	cat > Dockerfile <<EOF
+FROM scratch
+ADD rootfs.tar.xz /
+
+ENV ARCH=${ARCH} ALPINE_REL=${REL} DOCKER_REPO=${repo} ALPINE_MIRROR=${MIRROR}
+RUN ALPINE_GLIBC_BASE_URL="https://github.com/andyshinn/alpine-pkg-glibc/releases/download" && \
+    ALPINE_GLIBC_PACKAGE_VERSION="2.23-r1" && \
+    ALPINE_GLIBC_BASE_PACKAGE_FILENAME="glibc-$ALPINE_GLIBC_PACKAGE_VERSION.apk" && \
+    ALPINE_GLIBC_BIN_PACKAGE_FILENAME="glibc-bin-$ALPINE_GLIBC_PACKAGE_VERSION.apk" && \
+    ALPINE_GLIBC_I18N_PACKAGE_FILENAME="glibc-i18n-$ALPINE_GLIBC_PACKAGE_VERSION.apk" && \
+    apk add --no-cache --virtual=build-dependencies wget ca-certificates && \
+    wget \
+        "https://raw.githubusercontent.com/andyshinn/alpine-pkg-glibc/master/andyshinn.rsa.pub" \
+        -O "/etc/apk/keys/andyshinn.rsa.pub" && \
+    wget \
+        "$ALPINE_GLIBC_BASE_URL/$ALPINE_GLIBC_PACKAGE_VERSION/$ALPINE_GLIBC_BASE_PACKAGE_FILENAME" \
+        "$ALPINE_GLIBC_BASE_URL/$ALPINE_GLIBC_PACKAGE_VERSION/$ALPINE_GLIBC_BIN_PACKAGE_FILENAME" \
+        "$ALPINE_GLIBC_BASE_URL/$ALPINE_GLIBC_PACKAGE_VERSION/$ALPINE_GLIBC_I18N_PACKAGE_FILENAME" && \
+    apk add --no-cache \
+        "$ALPINE_GLIBC_BASE_PACKAGE_FILENAME" \
+        "$ALPINE_GLIBC_BIN_PACKAGE_FILENAME" \
+        "$ALPINE_GLIBC_I18N_PACKAGE_FILENAME" && \
+    \
+    rm "/etc/apk/keys/andyshinn.rsa.pub" && \
+    /usr/glibc-compat/bin/localedef --force --inputfile POSIX --charmap UTF-8 C.UTF-8 || true && \
+    echo "export LANG=C.UTF-8" > /etc/profile.d/locale.sh && \
+    \
+    apk del glibc-i18n && \
+    \
+    apk del build-dependencies && \
+    rm \
+        "$ALPINE_GLIBC_BASE_PACKAGE_FILENAME" \
+        "$ALPINE_GLIBC_BIN_PACKAGE_FILENAME" \
+        "$ALPINE_GLIBC_I18N_PACKAGE_FILENAME"
+
+ENV LANG=C.UTF-8
+EOF
+
+	# add qemu-user-static binary
+	if [ -n "${qemu_arch}" ]; then
+	    cat >> Dockerfile <<EOF
+
+# Add qemu-user-static binary for amd64 builders
+ADD x86_64_qemu-${qemu_arch}-static.tar.xz /usr/bin
+EOF
+	fi
+
+	# build
+	docker build -t "${repo}:${ARCH}-${REL}" .
+	docker run --rm "${repo}:${ARCH}-${REL}" /bin/sh -ec "echo Hello from Alpine !; set -x; uname -a; cat /etc/alpine-release"
+
+	# FIXME: tag latest
+    )
+done
